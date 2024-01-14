62835205ee5287bf4772f6e7f5f064d0c488c56f
Jonathan <sabza178@gmail.com>
2024-01-14 18:46:53 +0700

    add swagger dependency

---
+++ src/main/java/com/example/swagger/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.swagger.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> listUser(){
+        return new ResponseEntity<>(List.of("aorjor","anuchito"), HttpStatus.OK);
+    }
+}
