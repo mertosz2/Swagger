27d54abe19a98d9be0b0958d81ae9099e598dbbd
Jonathan <sabza178@gmail.com>
2024-01-14 19:16:20 +0700

    add POST method createUser

--- src/main/java/com/example/swagger/controller/UserController.java
+++ src/main/java/com/example/swagger/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.swagger.controller;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +17,11 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<String>> listUser(){
         return new ResponseEntity<>(List.of("aorjor","anuchito"), HttpStatus.OK);
+    }
+
+    @PostMapping
+    @ApiResponse(responseCode = "201", description = "User created")
+    public ResponseEntity<Integer> createUser(Integer id, String name){
+        return new ResponseEntity<>(id, HttpStatus.CREATED);
     }
 }
